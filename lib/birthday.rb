5fdafed7bc3b23357e4dde4df52f840f8e554151
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-07 19:55:31 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,6 +14,8 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each { |kids_name,age|
     if age < 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
+  else
+    puts "Tou are too old for this."
+    end
   }
 end
